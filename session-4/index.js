b74639f9f47a8f5f01929cac1563c483adc64305
Jennifer Lukanu <jenlukanu@gmail.com>
2022-01-24 21:41:45 +0000

    committed my work from session 4

--- session-4/index.js
+++ session-4/index.js
@@ -1,13 +1,47 @@
 // ****************************************************TASK 1 - OBJECTS****************************************************
+let personA = {
+    name: 'Michaelle',
+    age: '35',
+    location: 'Sheffield',
+    likes: 'running',
+};
+
+
+let personB = {
+    name: 'Patricia',
+    age: '31',
+    location: 'London',
+    likes: 'Films',
+}
 
 
 
+let personC = {
+    name: 'April',
+    likes: 'Art',
+    location: 'Birmingham',
+    age='23',
+};
 
 
+let personD = {
+    name: 'Ash',
+    age: '38',
+    location: 'Sweden',
+    likes: 'Baking',
+};
 
 
+function biography(person){
+//    console.log('Hi, my name is '+person.name+'. I am '+person.age+' years old, I live in '+person.location+
+    ' and I like '+person.likes+'!';
+    
 
-
+}
+biography(personA)
+biography(personB)
+biography(personC)
+biography(personD)
 
 
 
@@ -19,25 +53,28 @@
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+const x = 6;
+const y = 4;
 
-// const addition = x + y;
+const addition = x + y;
 
-// console.log("Addition: x + y " + addition);
+console.log("Addition: x + y " + addition);
 
-// const subtraction = x - y;
+const subtraction = x - y;
 
-// console.log("Subtraction: x - y " + subtraction);
+console.log("Subtraction: x - y " + subtraction);
 
-// const multiplication = x * y;
+const multiplication = x * y;
 
-// console.log("multiplication: x * y " + multiplication);
+console.log("multiplication: x * y " + multiplication);
 
-// const division = x * y;
+const division = x * y;
 
-// console.log("Division: x + y " + division);
+console.log("Division: x + y " + division);
 
+const addition3 = x + x + y
+const substractionb = x - x
+const substractionc = y - x
 
 
 
@@ -53,7 +90,11 @@
 
 
 // ****************************************************TASK 3****************************************************
+var age = 27;
+const drivingage = 16;
+const isoldenoughtodrive = age > drivingage
 
+console.log('amIoldenoughtodrive?  ' + isoldenoughtodrive)
 
 
 
