c961feaee830dcedbb6f49c97f403edac0a1de57
Jennifer Lukanu <jenlukanu@gmail.com>
2022-02-13 15:50:56 +0000

    session 5 task

--- session-5/index.js
+++ session-5/index.js
@@ -1,17 +1,55 @@
-function getGrade(total){
+function getGrade (total) {
+    let grade;
     
-        console.log("This grade is more than 90");  
-    
-if (total > 80){
-    console.log("This grade is more than 80");  
-}   else if (total >80 && total <=90) {
+    if (total > 90) {
+        grade = 'A+';
+    }  
+    else if (total > 80) {
+        grade = 'A';
+    }
+    else if (total > 70){
+        grade = 'B';
+    }
+    else if (total > 60) {
+        grade = 'C';
+    }
+    else if (total > 50) {
+        grade = 'D';
+    }
+    else if (total > 40) {
+        grade = 'E';
+    } 
+    else if (total > 30) {
+        grade = 'F';
+    }
+    else {
+        grade = 'Fail';
+    }
 
+    console.log('Your grade is Fail ' + 'This is unfortunate');
 }
 
-if (total > 70){
-    console.log("This grade is more than 70");  
-    else if (total >90 && total <=80) {
-
-if (total > 60){
-console.log("This grade is more than 60");  
-else if (total >90 && total <=70) 
+switch (total) {
+    case 90:
+    console.log("Your score is 90");
+    break;
+    case 80:
+        console.log("Your score is 80");
+        break;
+    case 70:
+        console.log("Your score is 70");
+        break;
+    case 60:
+        console.log("Your score os 60");
+        break;
+    case 50:
+        console.log("Your score is 50");
+        break;
+    case 40:
+            console.log("Not too bad")
+            break;
+    case 30:
+        console.log("You didn't meet the criteria");
+    default:
+        console.log("Not too sure");
+}
